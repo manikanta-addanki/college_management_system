fecad214c5d3e26b0dcb7918200c4f749bf52468
manikanta-addanki <addankivenkatamanikanta@gmail.com>
2025-12-09 09:28:54 +0530

    first commit

--- PRO_COLLEGE1/settings.py
+++ PRO_COLLEGE1/settings.py
@@ -73,12 +73,32 @@ WSGI_APPLICATION = 'PRO_COLLEGE1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+import os
+if os.environ.get("RENDER"):
+
+    DATABASES = {
+            "default":{
+                "ENGINE": "django.db.backends.mysql",
+                "NAME": os.environ.get("MYSQLDATABASE"),
+                "USER": os.environ.get("MYSQLUSER"),
+                "PASSWORD": os.environ.get("MYSQLPASSWORD"),
+                "HOST": os.environ.get("MYSQLHOST"),
+                "PORT": os.environ.get("MYSQPORT","3306"),
+            }
     }
-}
+
+
+
+
+
 
 
 # Password validation
